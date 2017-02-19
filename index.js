818f08ab8f82585687e742bb00ec0f46d877af9b
Barry Wallace <bwallace1231@Barrys-MacBook-Pro.local>
2017-02-19 10:11:24 -0500

    added class and constructor

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const config = require('./config.json');
+const assert = require('assert');
 const BattlefieldStats = require('battlefield-stats');
 const bf = new BattlefieldStats(config.API_KEY);
+
+class DiscordBFStats {
+  constructor(client){
+    assert(client,'Discord client missing');
+  }
+}
+
+module.exports = DiscordBFStats;
